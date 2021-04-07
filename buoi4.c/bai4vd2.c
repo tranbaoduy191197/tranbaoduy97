82aabd3fe8609775114903190d31a780dd0e35ce
tranbaoduy191197 <81789920+tranbaoduy191197@users.noreply.github.com>
2021-04-07 08:53:22 +0700

    Buoi4.c

---
+++ buoi4.c/bai4vd2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	putchar('H'); putchar('\n');
+	putchar('\t');
+	putchar('E'); putchar('\n');
+	putchar('\t');putchar ('\n');
+	putchar('L'); putchar ('\n');
+	putchar('t');putchar('\t');putchar('\t');
+	putchar('L');putchar('\t'); putchar ('\t');
+	putchar('\t');
+	putchar('\o');
+}
---
+++ buoi4.c/buoi4.c
@@ -0,0 +1,13 @@
+#include <stdio.h.>
+void main()
+{
+	int a = 10;
+	float b = 24.67892345;
+	char ch = 'A';
+	
+	printf("integer data = %d",a);
+	printf("float data = %d",b);
+	printf("Character = %d", ch);
+	printf("this prints the string");
+	printf("%s","this also prints a string");
+}
